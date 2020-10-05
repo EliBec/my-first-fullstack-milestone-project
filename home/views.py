cc010267ebffe090d5dd64324149ea13324986a8
Eliana Beckinsale <Elianageral@hotmail.com>
2020-10-05 02:12:20 +0000

    added home template folder and file

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def welcome(request):
+
+    welcome_msg = 'Welcome to Beck!'
+
+    # template = "home/index.html"
+
+    context = {
+        'welcome_msg': welcome_msg
+    }
+
+    return render(request, 'home/index.html', context)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .models import Product, Category,  Subcategory
+
 
 # Create your views here.
+def display_products(request):
+
+    products = Product.objects.all()
+
+    template = 'products/products.html'
+
+    context = {
+        products: 'products',
+    }
+
+    return render(request, template, context)
