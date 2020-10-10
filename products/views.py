ec46bfb26e37b919feeb93655c1d63fc00c60188
Eliana Beckinsale <Elianageral@hotmail.com>
2020-10-10 03:38:02 +0000

    added Breadcrumb functionality and queries for Search form as well as Menu and Submenu selection in products.html

--- products/views.py
+++ products/views.py
@@ -9,11 +9,14 @@ def display_all_products(request):
 
     products = Product.objects.all()
     search_query = None
+    category = None
+    subcategory = None
+    search_method = "menu_search"
 
     template = 'products/products.html'
 
-    # logic for Search form
     if request.GET:
+        # logic for Search form
         if 'srch_qry' in request.GET:
             search_query = request.GET['srch_qry']
             if not search_query:
@@ -25,10 +28,58 @@ def display_all_products(request):
                 Q(brand__icontains=search_query)
 
             products = products.filter(query_result)
+            search_method = "search_form"
+
+        if 'category' in request.GET:
+            cat_selected = request.GET['category']
+
+            """
+            get queryset from Product model for the category id
+            based on the selected subcategory name
+            code ref. https://overiq.com/django-1-11/django-orm-basics-part-2/
+            """
+            cat_products = Product.objects.filter(category__name=cat_selected)
+            # print(cat_products)
+            products = cat_products
+
+            category_search = Category.objects.filter(name=cat_selected)
+            # print(category_search)
+
+            category = category_search
+            print(category)
+
+            # category = cat_selected
+
+            search_method = "menu_search"
+
+        if 'subcategory' in request.GET:
+            subcat_selected = request.GET['subcategory']
+
+            """get queryset from Product model for the subcategory id
+            based on the selected subcategory name"""
+            subcat_products = \
+                Product.objects.filter(subcategory__name=subcat_selected)
+
+            products = subcat_products
+            print(products)
+
+            category_search = \
+                Category.objects.filter(subcategory__name=subcat_selected)
+            # print(category_search)
+
+            category = category_search
+            print(category)
+
+            subcategory = subcat_selected
+
+            search_method = "menu_search"
 
     context = {
         "products": products,
-        "search_term": search_query
+        "search_term": search_query,
+        "category": category,
+        "subcategory": subcategory,
+        "search_method": search_method
     }
 
     return render(request, template, context)
