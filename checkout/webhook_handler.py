a6b54b5827526f8e4858f0206f3a29daf2002da3
Eliana Beckinsale <Elianageral@hotmail.com>
2020-10-22 03:05:22 +0000

    added webhooks.py to listen to webhooks sent by Stripe

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,8 +6,26 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def __handle_event(self, event):
+    def handle_event(self, event):
+        """
+            handles stripe events other than succeeded or failed
+        """
+        return HttpResponse(
+            content=f'Unhandled received: {event["type"]}',
+            status=200)
 
+    def handle_payment_intent_succeeded(self, event):
+        """
+            handles stripe payment_intent.succeeded webhook
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+            handles stripe payment_intent.payment webhook
+        """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
