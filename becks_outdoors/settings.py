0061213ebe63388e97accea7d074cae7ed1a7365
Eliana Beckinsale <Elianageral@hotmail.com>
2020-11-02 07:13:16 +0000

    removed secret key and set debug

--- becks_outdoors/settings.py
+++ becks_outdoors/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-pvx0kn$rk84h5$yg3ay%0k1-0*_zp_!)!zkpdlf2=eq3s=2u4'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['becks-outdoors.herokuapp.com', 'localhost']
 
